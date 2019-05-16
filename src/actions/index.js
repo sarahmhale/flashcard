ba49ea611cb6b5e90e65775c1bac8495069930eb
Sarah <sarahhale95@gmail.com>
2019-05-16 14:13:51 +0200

    new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
-import { TOGGLE_THEME } from './actionTypes'
+import { TOGGLE_THEME, ADD_CARD } from './actionTypes'
 
 export const toggleTheme = theme => ({
     type: TOGGLE_THEME,
     payload: theme,
+})
+
+export const addCard = () => ({
+    type: ADD_CARD
 })
